78d14ee2657d3471de61c8bf69ab8c314189065a
Vaibhav MS <vaibhavsingh4141@gmail.com>
2024-05-17 01:04:00 +0530

    Path Updated

--- backend/app.js
+++ backend/app.js
@@ -17,11 +17,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
-app.use(express.static(path.join(__dirname, '../TEIFICCLIENT/build')));
+app.use(express.static(path.join(__dirname, '../../TEIFICCLIENT/build')));
 
   app.get('*', (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, '../', 'TEIFICCLIENT', 'build', 'index.html')
+      path.resolve(__dirname, '../','../', 'TEIFICCLIENT', 'build', 'index.html')
     )
   );
 // Route Import
