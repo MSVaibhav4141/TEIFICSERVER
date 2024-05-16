5387e52036d2ffa260045b49180adb250d123b28
Vaibhav MS <vaibhavsingh4141@gmail.com>
2024-05-17 00:49:45 +0530

    Setting get request

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const userRouter = require("./routes/userRoute");
 const orderRouter = require("./routes/orderRoute");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
+const path = require("path");
 const errorCatcher = require("./utils/errorCatcher");
 const cookieParser = require("cookie-parser");
 const cors = require('cors')
@@ -16,6 +17,13 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
+app.use(express.static(path.join(__dirname, '../TEIFICCLIENT/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'TEIFICCLIENT', 'build', 'index.html')
+    )
+  );
 // Route Import
 app.use("/VC1", router);
 app.use("/VC1", userRouter);
